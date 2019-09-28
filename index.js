6e0a8477b89023d9177c7afbcfb4c36c30e2e870
Folivi95 <joshua.folivi@gmail.com>
2019-09-28 16:12:18 +0100

    admin user api creation

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const redis = require('redis');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = express();
 
 //configure environment variables
 dotenv.config();
 
+//Importing Routes
+const adminRoute = require('./routes/auth/admin');
+const userRoute = require('./routes/auth/user');
 
-const app = express();
+//Middleware
+app.use(express.json());
+
+//connect to mongoDB
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect(process.env.DB_CONNECT,
+    { useNewUrlParser: true},
+    () => console.log('Successfully Connected to DB'));
+
+
+//Route Middleware
+app.use('/api/v1', adminRoute); //admin users route
+app.use('/api/v1', userRoute);  //normal users route
 
 app.listen(process.env.PORT, () => console.log(`Server listening on ${process.env.PORT}`));
